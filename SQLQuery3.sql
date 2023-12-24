413663c72151d6b42ba1c4e9688f5deceff53a3b
Sathmi Rajapaksha <devsadini@gmail.com>
2023-12-24 14:13:40 +0530

    commit2

---
+++ SQLQuery3.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE customerDetails(
+   customerId INT IDENTITY(1,1),
+   customerName VARCHAR(250) NOT NULL, 
+   customerEmail VARCHAR(250),
+   customerPhone VARCHAR(25) NOT NULL,
+   customerIdentityNo VARCHAR(25) NOT NULL,
+   PRIMARY KEY(customerId)
+   );
