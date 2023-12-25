c91b46bccc8280fb25345840ebad8ebf185f1d44
Shahla-AAFS <139673365+Shahla-AAFS@users.noreply.github.com>
2023-12-25 21:42:58 +0530

    Add files via upload

---
+++ C# project/Hotel Booking System/Hotel Booking System/Search.Designer.cs
@@ -0,0 +1,180 @@
+﻿
+namespace Hotel_Booking_System
+{
+    partial class Search
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.radioButtonID = new System.Windows.Forms.RadioButton();
+            this.radioButtonPhone = new System.Windows.Forms.RadioButton();
+            this.radioButtonName = new System.Windows.Forms.RadioButton();
+            this.buttonSearch = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBoxSearch = new System.Windows.Forms.TextBox();
+            this.dataGridViewSearchResult = new System.Windows.Forms.DataGridView();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewSearchResult)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.groupBox2);
+            this.groupBox1.Controls.Add(this.buttonSearch);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.textBoxSearch);
+            this.groupBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox1.Location = new System.Drawing.Point(0, 0);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(788, 226);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Search Customers";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.radioButtonID);
+            this.groupBox2.Controls.Add(this.radioButtonPhone);
+            this.groupBox2.Controls.Add(this.radioButtonName);
+            this.groupBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox2.Location = new System.Drawing.Point(42, 143);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(539, 55);
+            this.groupBox2.TabIndex = 6;
+            this.groupBox2.TabStop = false;
+            // 
+            // radioButtonID
+            // 
+            this.radioButtonID.AutoSize = true;
+            this.radioButtonID.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.radioButtonID.Location = new System.Drawing.Point(40, 23);
+            this.radioButtonID.Name = "radioButtonID";
+            this.radioButtonID.Size = new System.Drawing.Size(108, 20);
+            this.radioButtonID.TabIndex = 5;
+            this.radioButtonID.TabStop = true;
+            this.radioButtonID.Text = "By Identity No";
+            this.radioButtonID.UseVisualStyleBackColor = true;
+            // 
+            // radioButtonPhone
+            // 
+            this.radioButtonPhone.AutoSize = true;
+            this.radioButtonPhone.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.radioButtonPhone.Location = new System.Drawing.Point(387, 23);
+            this.radioButtonPhone.Name = "radioButtonPhone";
+            this.radioButtonPhone.Size = new System.Drawing.Size(108, 20);
+            this.radioButtonPhone.TabIndex = 5;
+            this.radioButtonPhone.TabStop = true;
+            this.radioButtonPhone.Text = "By Phone No ";
+            this.radioButtonPhone.UseVisualStyleBackColor = true;
+            // 
+            // radioButtonName
+            // 
+            this.radioButtonName.AutoSize = true;
+            this.radioButtonName.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.radioButtonName.Location = new System.Drawing.Point(229, 21);
+            this.radioButtonName.Name = "radioButtonName";
+            this.radioButtonName.Size = new System.Drawing.Size(82, 20);
+            this.radioButtonName.TabIndex = 5;
+            this.radioButtonName.TabStop = true;
+            this.radioButtonName.Text = "By Name";
+            this.radioButtonName.UseVisualStyleBackColor = true;
+            // 
+            // buttonSearch
+            // 
+            this.buttonSearch.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
+            this.buttonSearch.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(192)))));
+            this.buttonSearch.FlatAppearance.BorderSize = 2;
+            this.buttonSearch.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.buttonSearch.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonSearch.Location = new System.Drawing.Point(618, 155);
+            this.buttonSearch.Name = "buttonSearch";
+            this.buttonSearch.Size = new System.Drawing.Size(87, 38);
+            this.buttonSearch.TabIndex = 3;
+            this.buttonSearch.Text = "Search";
+            this.buttonSearch.UseVisualStyleBackColor = false;
+            this.buttonSearch.Click += new System.EventHandler(this.buttonSearch_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft YaHei", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(38, 45);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(354, 19);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Search Customers by ID , Name  or Phone Number :";
+            // 
+            // textBoxSearch
+            // 
+            this.textBoxSearch.Location = new System.Drawing.Point(42, 82);
+            this.textBoxSearch.Name = "textBoxSearch";
+            this.textBoxSearch.Size = new System.Drawing.Size(672, 22);
+            this.textBoxSearch.TabIndex = 1;
+  //          this.textBoxSearch.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // dataGridViewSearchResult
+            // 
+            this.dataGridViewSearchResult.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridViewSearchResult.Location = new System.Drawing.Point(67, 247);
+            this.dataGridViewSearchResult.Name = "dataGridViewSearchResult";
+            this.dataGridViewSearchResult.Size = new System.Drawing.Size(667, 173);
+            this.dataGridViewSearchResult.TabIndex = 4;
+            // 
+            // Search
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.dataGridViewSearchResult);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Search";
+            this.Text = "Search";
+//            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewSearchResult)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBoxSearch;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Button buttonSearch;
+        private System.Windows.Forms.DataGridView dataGridViewSearchResult;
+        private System.Windows.Forms.RadioButton radioButtonID;
+        private System.Windows.Forms.RadioButton radioButtonName;
+        private System.Windows.Forms.RadioButton radioButtonPhone;
+    }
+}
+
---
+++ C# project/Hotel Booking System/Hotel Booking System/Search.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+using System.Data;
+
+namespace Hotel_Booking_System
+{
+    public partial class Search : Form
+    {
+        //private string CONNECTION_STRING = "";
+        //SqlDataAdapter adpt;
+        //DataTable dt;
+        //SqlConnection conn = new SqlConnection();
+
+        public Search()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonSearch_Click(object sender, EventArgs e)
+        {
+            bool idSearchselelcted = radioButtonID.Checked;
+            bool nameSearchselelcted = radioButtonName.Checked;
+            bool phoneNoSearchselelcted = radioButtonPhone.Checked;
+            //conn.ConnectionString = CONNECTION_STRING;
+            //SqlCommand cmd = new SqlCommand();
+            //string textToSearch = textBoxSearch.Text.ToString();
+            if (idSearchselelcted)
+            {
+                MessageBox.Show("Identity search selected!");
+
+                // cmd.CommandText = "SELECT * FROM customerDetails WHERE identityNo ='" +textToSearch + "'";
+                adpt = new SqlDataAdapter("SELECT * FROM customerDetails WHERE identityNo = '" + textToSearch + "'", conn);
+                dt = new DataTable();
+                adpt.Fill(dt);
+                dataGridViewSearchResult.DataSource = dt;
+            }
+            else if(nameSearchselelcted)
+            {
+                MessageBox.Show("Name search selected!");
+                try
+                {
+                    adpt = new SqlDataAdapter("SELECT * FROM customerDetails WHERE customerName = '" + textToSearch + "'", conn);
+                    dt = new DataTable();
+                    adpt.Fill(dt);
+                    dataGridViewSearchResult.DataSource = dt;
+                }
+                catch(Exception ex)
+                {
+                    MessageBox.Show("ex.Message");
+                }
+            }
+            else if(phoneNoSearchselelcted)
+            {
+                MessageBox.Show("Phone search selected!");
+                adpt = new SqlDataAdapter("SELECT * FROM customerDetails WHERE customerPhone = %'" + textToSearch + "'%", conn);
+                dt = new DataTable();
+                adpt.Fill(dt);
+                dataGridViewSearchResult.DataSource = dt;
+            }
+
+        }
+
+       
+    }
+}
